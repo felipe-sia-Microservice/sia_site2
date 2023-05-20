b4d2d587cbbcbd8dad1a469ac1116a9eafe75994
philipsamporna <phelipsamporna@gmail.com>
2023-05-20 12:49:04 -0700

    sia2

---
+++ app/Models/User.php
@@ -0,0 +1,20 @@
+<?php
+
+    namespace App\Models;
+
+    use Illuminate\Database\Eloquent\Model;
+    
+    class User extends Model{
+    // name sa table
+        protected $table = 'tbluser';
+    // column sa table
+        protected $fillable = [
+            'username', 'password', 'gender'
+        ];
+
+    public $timestamps = false;
+        protected $primaryKey = 'user_id';
+        protected $hidden = [
+            'password'
+        ];
+    }
